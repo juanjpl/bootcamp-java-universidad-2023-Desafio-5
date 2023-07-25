4411bd8431214680523911c0710c70c684283117
Juan Lima <juan_jpl@hotmail.com>
2023-07-24 21:28:10 -0300

    update v1.0.3

--- js/condicionales.js
+++ js/condicionales.js
@@ -1,19 +1,39 @@
-
-
+// Calculadora!!!!!
+// ingresar dos valores 
+// ingresar la operación
+// mostrar el resultado
+// las operacione posibles son: +,-,/,*
+// si la operación no es valida, mostrar un cartel 
 
 const numero1 = Number(prompt('Ingrese numero 1:'));
 const numero2 = Number(prompt('Ingrese numero 2:'));
-const operacion = prompt('Ingrese la operacion a realizar:')
+const operacion = prompt('Ingrese la operacion a realizar (+,-,*,/)')
 
-
-if(operacion==='suma'){
-    alert(numero1+numero2)
-}else if(operacion==='resta') {
-    alert(numero1-numero2)
-}else if(operacion==='multiplicacion'){
-    alert(numero1*numero2)
-}else{
-if(numero2===0){
-    alert('Numero ingresado invalido')
+//primero todas las validaciones
+if(!numero1 || !numero2) {
+    alert('Números inválidos');
+    return;
 }
+
+if(operacion !== '+' 
+    &&  operacion !== '-' 
+        && operacion !== '/' 
+            &&  operacion !== '*' ) {
+                alert('operación inválida');
+                return;
+}
+
+if (operacion === '/' && numero2 === 0) {
+    alert('No se puede dividir entre cero')
+    return;
+}
+
+if (operacion === '+') {
+    alert(numero1 + numero2)
+} else if (operacion === '-') {
+    alert(numero1 - numero2)
+} else if (operacion === '*') {
+    alert(numero1 * numero2)
+} if (operacion === '/') {
+    alert(numero1 / numero2);
 }
